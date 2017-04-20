c18840439cf9b247c3e39e1e5afbae5f6ce57aa6
alec aivazis <alec@aivazis.com>
2017-04-20 01:06:11 -0700

    initial commit

---
+++ config/projectPaths.js
@@ -0,0 +1,33 @@
+/**
+ * Provides a single, consistent place for js files to get
+ * relevant paths, globs, etc pertaining to the project structure.
+ */
+
+// node imports
+var path = require('path')
+
+var rootDir = path.join(__dirname, '..')
+var configDir = path.join(rootDir, 'config')
+var buildDir = path.join(rootDir, 'build')
+var sourceDir = path.join(rootDir, 'src')
+var clientEntry = path.join(sourceDir, 'example.js')
+var clientBuild = path.join(buildDir, 'client.js')
+var indexTemplate = path.join(sourceDir, 'index.html')
+
+module.exports = {
+    // directories
+    rootDir: rootDir,
+    sourceDir: sourceDir,
+    buildDir: buildDir,
+    // entry points
+    clientEntry: clientEntry,
+    // built files
+    clientBuild: clientBuild,
+    indexTemplate: indexTemplate,
+    // globs
+    clientBuildGlob: path.join(clientBuild, '*'),
+    testGlob: path.join(sourceDir, "**", "*test.js"),
+    // configuration files
+    webpackConfig: path.join(configDir, 'webpack.js'),
+    babelConfig: path.join(configDir, 'babel.json'),
+}
---
+++ config/webpack.js
@@ -0,0 +1,102 @@
+// webpack imports
+var webpack = require('webpack')
+var process = require('process')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+// local imports
+var projectPaths = require('./projectPaths')
+
+// default to using development configuration
+var devtool = ''
+
+var entry = [projectPaths.clientEntry]
+
+// the initial set of plugins
+var plugins = [
+    new HtmlWebpackPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: process.env.NODE_ENV || 'dev',
+    }),
+]
+// if we are building for production
+if (process.env.NODE_ENV === 'production') {
+    // use production plugins
+    plugins.push(
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                screw_ie8: true,
+                conditionals: true,
+                unused: true,
+                comparisons: true,
+                sequences: true,
+                dead_code: true,
+                evaluate: true,
+                if_return: true,
+                join_vars: true,
+            },
+            output: {
+                comments: false,
+            },
+        })
+    )
+} else {
+    // use source maps
+    devtool = 'inline-source-map'
+
+    // add the webpack dev server config
+    entry = [
+        'webpack-dev-server/client?http://0.0.0.0:8080',
+        'webpack/hot/only-dev-server',
+    ].concat(entry)
+}
+
+// export webpack configuration object
+module.exports = {
+    entry: entry,
+    output: {
+        path: projectPaths.buildDir,
+        filename: 'client.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                include: projectPaths.sourceDir
+            }, {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            }, {
+                test: /\.(png|jpg|ttf)$/,
+                loader: 'url-loader',
+                query: {limit: 10000000},
+            },
+
+        ],
+    },
+    resolve: {
+        extensions: ['.jsx', '.js'],
+        modules: [
+            projectPaths.sourceDir,
+            projectPaths.rootDir,
+            'node_modules',
+        ],
+    },
+    plugins: plugins,
+    devtool: devtool,
+    devServer: {
+      proxy: {
+        '/latex': {
+          target: "http://104.196.244.203"
+        }
+      },
+    }
+}
+
+
+// end of file
