fbaef9623a2980411c29741259ce555d94cc595e
alec aivazis <alec@aivazis.com>
2017-05-17 14:52:10 -0700

    fixed errant import

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export Example from './Example'
 export Blueprint from './Blueprint'
-export Workbench from './Workbench'
+export Workshop from './Workshop'
 export Category from './Category'
 export PropValue from './PropValue'
 export PropTable from './PropTable'
